d2e529bcf3cc99b2a94296c1c806feb42a4db817
rimaakter1 <rimac201262@gmail.com>
2025-02-26 02:31:56 +0600

    added dark and light layout available camp and service camp

--- src/pages/AllServices/AllServices.jsx
+++ src/pages/AllServices/AllServices.jsx
@@ -22,23 +22,25 @@ const AllServices = () => {
         return <Loading />;
     }
 
-
     return (
-        <div className='mt-32 mb-44 '>
-            <div className='lg:w-10/12 w-11/12 mx-auto '>
+        <div className='pt-32 bg-white dark:bg-gray-700'>
+            <div className='lg:w-10/12 w-11/12 mx-auto'>
                 <div className='text-center mb-12'>
-                    <h1 className='text-4xl font-bold font-Parkinsans text-blue-700'>Comprehensive Healthcare, <br /> Anytime You Need</h1>
-                    <p className='text-gray-400 mt-5'>Our medical camp provides a full range of healthcare services, ensuring  accessible and reliable treatment <br /> for everyone. Whether you need preventive care, expert consultations, or emergency support, our dedicated team is here to help. </p>
+                    <h1 className='text-4xl font-bold font-Parkinsans text-blue-700 dark:text-blue-400'>
+                        Comprehensive Healthcare, <br /> Anytime You Need
+                    </h1>
+                    <p className='text-gray-400 dark:text-gray-300 mt-5'>
+                        Our medical camp provides a full range of healthcare services, ensuring accessible and reliable treatment <br />
+                        for everyone. Whether you need preventive care, expert consultations, or emergency support, our dedicated team is here to help.
+                    </p>
                 </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 pb-20">
                     {allServices.map((service) => (
-                        <ServiceCard service={service}></ServiceCard>
+                        <ServiceCard key={service._id} service={service} />
                     ))}
                 </div>
             </div>
 
-            <Work></Work>
-            <FeedbackAndRatings></FeedbackAndRatings>
         </div>
     );
 };
--- src/pages/AvailableCamps/AvailableCamps.jsx
+++ src/pages/AvailableCamps/AvailableCamps.jsx
@@ -37,14 +37,16 @@ const AvailableCamps = () => {
         });
 
     if (isLoading) {
-        return <Loading></Loading>;
+        return <Loading />;
     }
 
     return (
-        <section className="bg-gray-50 py-12 px-4 md:px-16 lg:px-32 mt-20">
+        <section className="bg-gray-50 dark:bg-gray-950 py-12 px-4 md:px-16 lg:px-32 mt-20">
             <header className="text-center mb-8">
-                <h2 className="text-4xl font-bold text-blue-700">Explore Our Available Medical Camps</h2>
-                <p className="text-gray-600 w-9/12 mx-auto mt-4">
+                <h2 className="text-4xl font-bold text-blue-700 dark:text-blue-400">
+                    Explore Our Available Medical Camps
+                </h2>
+                <p className="text-gray-600 dark:text-gray-300 w-9/12 mx-auto mt-4">
                     Discover medical camps tailored for diverse health needs, offering expert care, accessible
                     locations, and affordable fees. Browse now to secure your spot for quality healthcare!
                 </p>
@@ -54,13 +56,13 @@ const AvailableCamps = () => {
                 <input
                     type="text"
                     placeholder="Search camps by name..."
-                    className="border border-gray-300 rounded-lg px-4 py-2 w-full md:w-1/3"
+                    className="border border-gray-300 dark:border-gray-700 rounded-lg px-4 py-2 w-full md:w-1/3 dark:bg-gray-700 dark:text-gray-300"
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
                 />
 
                 <select
-                    className="border border-gray-300 rounded-lg px-4 py-2"
+                    className="border border-gray-300 dark:border-gray-700 rounded-lg px-4 py-2 dark:bg-gray-700 dark:text-gray-300"
                     value={sortCriteria}
                     onChange={(e) => setSortCriteria(e.target.value)}
                 >
@@ -71,7 +73,7 @@ const AvailableCamps = () => {
                 </select>
 
                 <button
-                    className="bg-blue-600 text-white px-4 py-2 rounded-lg"
+                    className="bg-blue-600 text-white px-4 py-2 rounded-lg dark:bg-blue-500 dark:hover:bg-blue-600"
                     onClick={() => setIsTwoColumn((prev) => !prev)}
                 >
                     {isTwoColumn ? "Switch to Three-Column" : "Switch to Two-Column"}
