9808da8cacfbfe870cc468ff164389605a157e5f
rimaakter1 <rimac201262@gmail.com>
2025-02-25 22:30:31 +0600

    fixed cards info

--- src/components/CampCard/CampCard.jsx
+++ src/components/CampCard/CampCard.jsx
@@ -9,33 +9,16 @@ const CampCard = ({ camp }) => {
                 alt={camp.name}
                 className="h-48 w-full object-cover"
             />
-            <div className="p-4 flex-grow flex flex-col">
+            <div className="p-4 flex-grow flex flex-col space-y-3">
                 <h3 className="text-xl font-semibold text-blue-700">{camp.name}</h3>
-                <p className="text-gray-700 mt-2">
-                    <span className="font-semibold">Fees:</span> ${camp.Fees}
-                </p>
-                <p className="text-gray-700 mt-1">
-                    <span className="font-semibold">Date & Time:</span>{" "}
-                    {new Date(camp.time).toLocaleString()}
-                </p>
-                <p className="text-gray-700 mt-1">
-                    <span className="font-semibold">Location:</span> {camp.location}
-                </p>
-                <p className="text-gray-700 mt-1">
-                    <span className="font-semibold">Healthcare Professional:</span>{" "}
-                    {camp.professionalName}
-                </p>
-                <p className="text-gray-700 mt-1 mb-4">
-                    <span className="font-semibold">Participants:</span>{" "}
-                    {camp.participantCount}
-                </p>
+                <p>Details: {camp.details}</p>
 
                 <div className="mt-auto w-full">
                     <Link
                         to={`/camp-details/${camp._id}`}
                         className="bg-blue-600 text-white p-3 font-bold rounded-lg w-full block text-center"
                     >
-                        Details
+                        See More
                     </Link>
                 </div>
             </div>
